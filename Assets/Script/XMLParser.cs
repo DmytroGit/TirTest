39632d46c3119ba2d72585ce88c061b075cdb3da
DmytroOleksandrovych <@mail>
2018-07-11 14:50:37 +0300

    no message

--- Assets/Script/XMLParser.cs
+++ Assets/Script/XMLParser.cs
@@ -39,20 +39,47 @@ namespace Tir
 
         public void Generate(XElement root)
         {
+
             foreach(XElement instance in root.Elements("type"))
             {
                 int x = int.Parse(instance.Attribute("count").Value);
 
                 string s = instance.Value;
 
+                TestImg(x, s);
+
                 Debug.Log(x + " - " + s + " **** ");
             }
         }
 
         //////////////////////////
-        public void TestImg()
+        public void TestImg(int x, string s)
         {
+            GameObject Spawn = Instantiate(Resources.Load("Spawn"), Vector3.zero, Quaternion.identity) as GameObject;
 
+            Spawn.GetComponent<Panelika>().SetText(x);
+
+            if(s == "Cube")
+            {
+                Spawn.GetComponent<Panelika>().ePlayerObject = EPlayerObject.Cube;
+            }
+            if(s == "Capsule")
+            {
+                Spawn.GetComponent<Panelika>().ePlayerObject = EPlayerObject.Capsule;
+            }
+            if(s == "Cylinder")
+            {
+                Spawn.GetComponent<Panelika>().ePlayerObject = EPlayerObject.Cylinder;
+            }
+            if(s == "Sphere")
+            {
+                Spawn.GetComponent<Panelika>().ePlayerObject = EPlayerObject.Sphere;
+            }
+
+
+
+
+            Spawn.transform.SetParent(GameObject.Find("Canvas").transform);
         }
     }
 }
